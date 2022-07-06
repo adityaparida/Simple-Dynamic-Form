1ef0e43b6813f8bae46824512ff3c29ad2165956
aditya parida <adityaparida9@gmail.com>
2022-07-06 18:14:08 +0530

    Complete Task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'sample-form';
+export class AppComponent implements OnInit{
+
+  userForm: FormGroup;
+  userArray: FormArray;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.userForm = this.formBuilder.group({
+      userArray: this.formBuilder.array([this.createUser()])
+    });
+  }
+
+  createUser() {
+    return this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      phone: ['', Validators.required]
+    });
+  }
+  
+  addUser() {
+    this.userArray = this.userForm.get('userArray') as FormArray;
+    this.userArray.push(this.createUser());
+  }
+
+  removeUser(id: number) {
+    this.userArray = this.userForm.get('userArray') as FormArray;
+    this.userArray.removeAt(id);
+  }
+
+  moveUser(id: number, shift) {
+    this.userArray = this.userForm.get('userArray') as FormArray;
+    const user = this.userArray.at(id);
+    this.userArray.removeAt(id);
+    this.userArray.insert(id + shift, user);
+  }
+
+  resetForm() {
+    this.userForm.reset();
+  }
+
+  onSubmit() {
+    console.log(this.userForm.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
